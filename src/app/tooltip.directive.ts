7a5ebba734cfb6ec888acdf43cf7a7b4e3740bf3
Andrei Voicu <dreivoicu@gmail.com>
2020-07-04 17:38:21 +0300

    intersection observer

--- src/app/tooltip.directive.ts
+++ src/app/tooltip.directive.ts
@@ -1,12 +1,19 @@
-import { Directive, Input, ElementRef, NgZone, AfterViewInit } from '@angular/core';
+import { Directive, Input, ElementRef, NgZone, AfterViewInit, OnDestroy } from '@angular/core';
 import tippy, { Instance } from 'tippy.js';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { inView } from './intersectionObserver';
 
 @Directive({
   selector: '[tooltip]'
 })
-export class TooltipDirective implements AfterViewInit {
-  private instance!: Instance;
+export class TooltipDirective implements AfterViewInit, OnDestroy {
+  private instance!: Instance | null;
   private _content!: string;
+
+  private destroy = new Subject();
+  private destroy$ = this.destroy.asObservable();
 
   get content(): string {
     return this._content;
@@ -25,12 +32,25 @@ export class TooltipDirective implements AfterViewInit {
   ) { }
 
   ngAfterViewInit(): void {
-    this.zone.runOutsideAngular(() => {
-      this.instance = tippy(this.host.nativeElement, {
-        content: this.content,
-        placement: 'auto-end'
-      });
+    inView(this.host.nativeElement)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((isInView) => {
+        if (isInView && !this.instance) {
+          this.zone.runOutsideAngular(() => {
+            this.instance = tippy(this.host.nativeElement, {
+              content: this.content,
+              placement: 'auto-end'
+            });
+          });
+        } else if (this.instance) {
+          this.instance.destroy();
+          this.instance = null;
+        }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
   }
 
 }
